fdffbc23080db6026d82eee51c60ae70f530723e
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2023-07-01 19:13:08 +0300

    Exercise 9.5

--- calculator/index.ts
+++ calculator/index.ts
@@ -1,9 +1,30 @@
 import express from 'express';
+import {calculateBmi} from './bmiCalculator';
+
+
+ //import calculateBmi from './bmiCalculator';
 
 const app = express();
 
 app.get('/hello',(_req,res) => {
     res.send('Hello Full Stack!');
+})
+
+
+app.get('/bmi',(req,res) => {    
+
+
+
+    if (!req.query.weigth || isNaN(Number(req.query.weigth))|| !req.query.heigth || isNaN(Number(req.query.heigth)) ){
+        res.status(400).send('malformatted parameters');
+    } else {
+        res.send( {
+            weigth: req.query.weigth,
+            heigth: req.query.heigth,
+            bmi: calculateBmi(Number(req.query.heigth),Number(req.query.weigth)),
+        });
+    }
+    
 })
 
 const PORT = 3003;
